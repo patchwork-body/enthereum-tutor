f39b86189e03046cbd936a5847b3177763f90d60
patchwork-body <gugfug1@gmail.com>
2021-09-04 16:47:21 +0300

    init home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import { AppProps } from 'next/app'
-import 'tailwindcss/tailwind.css'
+import { AppProps } from 'next/app';
+import 'tailwindcss/tailwind.css';
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default App
+export default App;
